43083f69aa889b37f4cde5b0fa0fe3dd3c91d4fc
Арина Канзафарова <marina.kanzafarova@gmail.com>
2025-04-08 12:18:19 +0000

    add: leaderboard

--- plov/leaderboard/views.py
+++ plov/leaderboard/views.py
@@ -3,12 +3,23 @@ import django.shortcuts
 import users.models
 
 
-def rep_leaderboard(request):
+def leaderboard(request):
     user_profiles = users.models.UserProfile.objects.all()
     leaderboard = user_profiles.order_by('-reputation_points')
 
     return django.shortcuts.render(
         request,
-        'statistic/rep_leaderboard.html',
+        'leaderboard/leaderboard.html',
+        {'leaderboard': leaderboard},
+    )
+
+
+def rep_leaderboard(request):
+    user_profiles = users.models.UserCourse.objects.all()
+    leaderboard = user_profiles.order_by('-rating')
+
+    return django.shortcuts.render(
+        request,
+        'leaderboard/rep_leaderboard.html',
         {'leaderboard': leaderboard},
     )
